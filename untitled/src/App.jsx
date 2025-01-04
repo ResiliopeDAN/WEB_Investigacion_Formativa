0da40cfe4e4563140def11c2bad55c00b338983b
ResiliopeDAN <danielqqq555@gmail.com>
2025-01-04 15:01:11 -0500

    CRUD_countries_SUPABASE
    Aquí se tiene un prototipado simple y comentado para las cuatro operaciones de CRUD en supabase, se espera entregar un prototipo con uso de función AUTH se supabase para la proxima vez

---
+++ untitled/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+import CountriesCRUD from "./CountriesCRUD.jsx";
+
+function App() {
+    return(
+        <div>
+            <CountriesCRUD/>
+        </div>
+    )
+}
+
+export default App
