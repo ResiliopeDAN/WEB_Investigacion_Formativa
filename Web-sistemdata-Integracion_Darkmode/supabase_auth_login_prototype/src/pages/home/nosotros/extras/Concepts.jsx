d27a99bc2ff96a57083d4a0aabc4ef845b49e97a
ResiliopeDAN <danielqqq555@gmail.com>
2025-04-10 17:54:38 -0500

    Update Concepts.jsx

--- Web-sistemdata-Integracion_Darkmode/supabase_auth_login_prototype/src/pages/home/nosotros/extras/Concepts.jsx
+++ Web-sistemdata-Integracion_Darkmode/supabase_auth_login_prototype/src/pages/home/nosotros/extras/Concepts.jsx
@@ -6,12 +6,12 @@ export default function Concepts() {
             <Flex divideColor={{ lg: 'black', _dark: 'gray' }}  my='5px' px={{ lg: 'full', base: '20px' }} gap={{ lg: "200px", base: "20px" }} display={{ lg: 'flex', base: 'grid'  }} py="40px">
             <Box   maxW="400px" >
                 <Heading textAlign="center" color={{base:"#072C51", _dark:"#CDF120"}}>Visión</Heading>
-                <Text>jdsoaonfoisandoinasosknsfknsnfksnnssdsdsdkfnsdknfdskdkadmkasndkasdkasndkassndkasdjsnadjassnidn teffy y edison / Aldo y Gina</Text>
+                <Text>Curabitur ultrices tortor ligula, sit amet fringilla tortor tristique at. Fusce feugiat nunc mi, non consequat ex tempus at. Aenean mi risus, gravida a tortor ut, efficitur finibus ipsum. Ut ultrices eros ante, vulputate dictum leo consequat a. Sed id mattis elit, ut egestas ligula. Fusce venenatis velit accumsan orci aliquam, nec consequat sem fringilla. Vestibulum gravida ipsum a ipsum dictum, sit amet imperdiet mi mattis.</Text>
             </Box>
 <Separator orientation={{ base: "horizontal", lg: "vertical" }} size="lg" />
             <Box maxW="400px">
                 <Heading textAlign="center" color={{base:"#072C51", _dark:"#CDF120"}} >Misión</Heading>
-                <Text>jdsoaonfoisandoinasoisdasjndasjndjkasndjkasnjkdkaslndlkasndlkasndlkandsdafsasdjnasjdjsaadnskandkasn alvis y betsy / Aldo y Gina ...... Ay el amor, el amor</Text>
+                <Text>Curabitur ultrices tortor ligula, sit amet fringilla tortor tristique at. Fusce feugiat nunc mi, non consequat ex tempus at. Aenean mi risus, gravida a tortor ut, efficitur finibus ipsum. Ut ultrices eros ante, vulputate dictum leo consequat a. Sed id mattis elit, ut egestas ligula. Fusce venenatis velit accumsan orci aliquam, nec consequat sem fringilla. Vestibulum gravida ipsum a ipsum dictum, sit amet imperdiet mi mattis.</Text>
             </Box>
             </Flex>
 
